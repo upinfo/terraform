d003ca5088745e60964b7a4411e276f392bac14a
Morlu Holder <morluholder2@gmail.com>
2021-11-21 18:21:33 -0500

    4commit subnets

--- vpc.tf
+++ vpc.tf
@@ -17,6 +17,8 @@ resource "aws_vpc" "morlu_vpc" {
     }
   
 }
+# Create Internet-Gateway
+
 resource "aws_internet_gateway" "morlu-vpc-igw" {
     vpc_id = aws_vpc.morlu_vpc.id
     tags = {
@@ -25,3 +27,52 @@ resource "aws_internet_gateway" "morlu-vpc-igw" {
     }
   
 }
+
+# Create Public-Subnets
+
+resource "aws_subnet" "public-morlu-SN-1a" {
+    vpc_id = aws_vpc.morlu_vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = "true"
+    tags = {
+      Name = "public-morlu-SN-1a"
+    }
+  
+}
+
+resource "aws_subnet" "public-morlu-SN-1b" {
+    vpc_id = aws_vpc.morlu_vpc.id
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = "true"
+    tags = {
+      Name = "public-morlu-SN-1b"
+    }
+  
+}
+
+# Create private-Subnets
+
+resource "aws_subnet" "private-morlu-SN-1c" {
+    vpc_id = aws_vpc.morlu_vpc.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "us-east-1d"
+    map_public_ip_on_launch = "false"
+    tags = {
+      Name = "private-morlu-SN-1c"
+    }
+  
+}
+
+resource "aws_subnet" "private-morlu-SN-1d" {
+    vpc_id = aws_vpc.morlu_vpc.id
+    cidr_block = "10.0.4.0/24"
+    availability_zone = "us-east-1d"
+    map_public_ip_on_launch = "false"
+    tags = {
+      Name = "private-morlu-SN-1d"
+    }
+  
+}
+
