1f534b8cd8927c164a89aeb73ec21496f8c610c8
Morlu Holder <morluholder2@gmail.com>
2021-11-21 11:34:47 -0500

    2 commit created vpc

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+# Define VPC Variable
+
+variable "aws-vpc-cidr" {
+    type = string
+    default = "10.0.0.0/16"
+  
+}
+
+# Create VPC
+
+resource "aws_vpc" "morlu_vpc" {
+    cidr_block = var.aws-vpc-cidr
+    instance_tenancy = "default"
+    tags = {
+      Name = "morlu_vpc"
+      Terraform = "True"
+    }
+  
+}
